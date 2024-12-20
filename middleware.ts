9c0e8d14aa10fbaaa7375e61b1005d02de6f8588
Ahmad Alhomsi <tursun3414@gmail.com>
2024-12-20 23:45:12 +0300

    Securing Routes

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,61 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
- 
-export default clerkMiddleware()
- 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkClient } from '@clerk/nextjs/server'
+
+
+
+
+// const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/forum(.*)'])
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth()
+  const client = await clerkClient()
+
+  const { pathname } = req.nextUrl
+  const method = req.method
+  console.log('Pathname:', pathname);
+  console.log('Method:', method);
+
+  if ((pathname === '/api/scholarship-status' && method === 'GET') ||
+    (pathname === '/api/uploadogrbelge' && method === 'POST') ||
+    (pathname === '/api/check-tc-kimlik-no' && method === 'POST') ||
+    (pathname === '/api/scholarship-applications' && method === 'POST') ||
+    (pathname === '/api/uploadnotort' && method === 'POST') ||
+    (pathname === '/api/uploadburs' && method === 'POST') ||
+    (pathname === '/api/uploadnufuz' && method === 'POST') ||
+    (pathname === '/api/uploadogrbelge' && method === 'POST') ||
+    (pathname === '/api/uploadtotal' && method === 'POST')
+  ) {
+    return
+  }
+
+  console.log('User ID:', userId);
+
+  if (pathname.startsWith('/api') || pathname.startsWith('/admin')) {
+    console.log('API or Admin Route');
+
+    if (userId) {
+      console.log('User ID:', userId);
+      const user = await client.users.getUser(userId)
+      const publicMetadata = user.publicMetadata
+
+      console.log('User Public Metadata:', publicMetadata)
+      return
+    }
+    else {
+      console.log('Redirecting to Sign In');
+
+      return redirectToSignIn()
+    }
+  }
+
+})
+
 export const config = {
   matcher: [
-    '/((?!.*\\..*|_next).*)', // Don't run middleware on static files
-    '/', // Run middleware on index page
-    '/(api|trpc)(.*)'], // Run middleware on API routes
-};
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
